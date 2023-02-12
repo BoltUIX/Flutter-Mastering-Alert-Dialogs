8a46f8c1f214f6d742d42dd3ae74ce17b13fa5d2
harishankar <harishankar@zeddigital.net>
2023-02-12 12:36:09 +0530

    * Snackbar * Custom Snackbar

--- lib/typography.dart
+++ lib/typography.dart
@@ -13,7 +13,8 @@ class TextStyleExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+     // padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(1.0),
       child: Text(name, style: style.copyWith(letterSpacing: 1.0)),
     );
   }
